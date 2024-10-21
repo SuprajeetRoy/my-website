832195136ad2c048769084c7846b859ac91f89bb
SuprajeetRoy <suprajeetroy17@gmail.com>
2024-10-21 20:40:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Optional interactive elements can be added here
+// For instance, smooth scrolling to sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
